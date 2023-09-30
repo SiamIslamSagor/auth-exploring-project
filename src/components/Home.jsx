95c2da0955099b55adaaaae5da817e35c6f6c294
SiamIslamSagor <msiamislam12345@gmail.com>
2023-09-30 20:10:28 +0600

    add user observer state

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,7 @@
-import { useContext } from "react";
-import { AuthContext } from "../providers/AuthProvider";
-
 const Home = () => {
-  const data = useContext(AuthContext);
-  console.log(data);
   return (
     <div>
-      <h2 className="text-3xl">This is Home for: {data.name}</h2>
+      <h2 className="text-3xl">This is Home</h2>
     </div>
   );
 };
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,41 @@
-import { createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import auth from "../firebase/firebase.config";
 
 //
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-  const authInfo = { name: "Mr. Jodu Miya" };
+  const [user, setUser] = useState(null);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+      console.log(
+        "observing current user inside useEffect of AuthProvider",
+        currentUser
+      );
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = { user, createUser, signInUser };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
