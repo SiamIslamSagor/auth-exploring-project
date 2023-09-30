f8d0f64f1461898df3406dfcbd94eb781a08238d
SiamIslamSagor <msiamislam12345@gmail.com>
2023-09-30 17:25:06 +0600

    create basic setup for project

---
+++ src/components/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <h3 className="text-2xl">This is register page </h3>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAK2jlHFaPPqzqXKhlt0BNJsB7WBbDhc3E",
+  authDomain: "auth-exploring-project.firebaseapp.com",
+  projectId: "auth-exploring-project",
+  storageBucket: "auth-exploring-project.appspot.com",
+  messagingSenderId: "1069795554375",
+  appId: "1:1069795554375:web:b1814af7d3b8faab6cad62",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
